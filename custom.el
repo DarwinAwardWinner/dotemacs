c9dfa37313315fe7ea4cf2240a998baefeaf8c90
Ryan C. Thompson <rct@thompsonclan.org>
2014-11-16 13:55:52 -0800

    New emacs formats custom.el differently

--- custom.el
+++ custom.el
@@ -13,7 +13,9 @@
  '(autopair-global-mode t)
  '(backup-directory-alist (quote (("." . "~/.emacs.d/persistence/backups"))))
  '(bs-default-configuration "all")
- '(completion-styles (quote (basic partial-completion emacs22 substring initials)))
+ '(completion-styles
+   (quote
+    (basic partial-completion emacs22 substring initials)))
  '(cperl-break-one-line-blocks-when-indent nil)
  '(cperl-close-paren-offset -4)
  '(cperl-highlight-variables-indiscriminately t)
@@ -26,21 +28,43 @@
  '(desktop-path (quote ("~/.emacs.d/persistence" "~")))
  '(desktop-save-mode t)
  '(diff-switches "-U3")
- '(diminished-minor-modes (quote ((volatile-highlights-mode . "") (eldoc-mode . "") (filladapt-mode . "") (highlight-parentheses-mode . "") (autopair-mode . "") (auto-complete-mode . "") (cua-mode . "") (ws-butler-mode . "") (undo-tree-mode . "") (anzu-mode . "") (auto-dim-other-buffers-mode . "") (highlight-changes-mode . "") (magit-auto-revert-mode . "") (git-gutter+-mode . "") (ws-butler-mode . ""))))
+ '(diminished-minor-modes
+   (quote
+    ((volatile-highlights-mode . "")
+     (eldoc-mode . "")
+     (filladapt-mode . "")
+     (highlight-parentheses-mode . "")
+     (autopair-mode . "")
+     (auto-complete-mode . "")
+     (cua-mode . "")
+     (ws-butler-mode . "")
+     (undo-tree-mode . "")
+     (anzu-mode . "")
+     (auto-dim-other-buffers-mode . "")
+     (highlight-changes-mode . "")
+     (magit-auto-revert-mode . "")
+     (git-gutter+-mode . "")
+     (ws-butler-mode . ""))))
  '(dired-auto-revert-buffer (quote dired-directory-changed-p))
- '(dired-omit-extensions (quote (".o" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo" ".idx" ".lof" ".lot" ".glo" ".blg" ".bbl" ".cp" ".cps" ".fn" ".fns" ".ky" ".kys" ".pg" ".pgs" ".tp" ".tps" ".vr" ".vrs")))
+ '(dired-omit-extensions
+   (quote
+    (".o" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo" ".idx" ".lof" ".lot" ".glo" ".blg" ".bbl" ".cp" ".cps" ".fn" ".fns" ".ky" ".kys" ".pg" ".pgs" ".tp" ".tps" ".vr" ".vrs")))
  '(dired-omit-files "^\\.?#\\|^\\.$\\|\\.DS_Store$")
  '(dired-use-ls-dired (quote unspecified))
  '(echo-keystrokes 0.05)
  '(el-get-is-lazy nil)
  '(eldoc-idle-delay 0.05)
- '(elfeed-feeds (quote ("http://www.rna-seqblog.com/feed/" "http://planet.emacsen.org/atom.xml" "http://feeds2.feedburner.com/webupd8" "http://owncloudtest.blogspot.com/feeds/posts/default" "http://feeds.feedburner.com/d0od" "http://blog.dustinkirkland.com/feeds/posts/default" "http://www.aaronsw.com/2002/feeds/pgessays.rss" "http://steve-yegge.blogspot.com/feeds/posts/default" "http://www.phdcomics.com/gradfeed.php" "http://blag.xkcd.com/feed/" "http://www.bioconductor.org/rss/new_packages.rss" "http://newsboyhat.co.uk/?feed=rss2" "http://www.modernperlbooks.com/mt/atom.xml" "http://what-if.xkcd.com/feed.atom" "http://www.oak-tree.us/blog/index.php/feed" "http://newsboyhat.livejournal.com/data/rss" "http://sarahsgeologicadventures.blogspot.com/feeds/posts/default")))
+ '(elfeed-feeds
+   (quote
+    ("http://www.rna-seqblog.com/feed/" "http://planet.emacsen.org/atom.xml" "http://feeds2.feedburner.com/webupd8" "http://owncloudtest.blogspot.com/feeds/posts/default" "http://feeds.feedburner.com/d0od" "http://blog.dustinkirkland.com/feeds/posts/default" "http://www.aaronsw.com/2002/feeds/pgessays.rss" "http://steve-yegge.blogspot.com/feeds/posts/default" "http://www.phdcomics.com/gradfeed.php" "http://blag.xkcd.com/feed/" "http://www.bioconductor.org/rss/new_packages.rss" "http://newsboyhat.co.uk/?feed=rss2" "http://www.modernperlbooks.com/mt/atom.xml" "http://what-if.xkcd.com/feed.atom" "http://www.oak-tree.us/blog/index.php/feed" "http://newsboyhat.livejournal.com/data/rss" "http://sarahsgeologicadventures.blogspot.com/feeds/posts/default")))
  '(enable-recursive-minibuffers t)
  '(ess-default-style (quote C++))
  '(ess-fancy-comments nil)
  '(ess-use-tracebug nil)
  '(esup-run-as-batch-p t)
- '(exec-path (quote ("~/bin" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin" "/opt/local/bin" "/usr/local/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin")))
+ '(exec-path
+   (quote
+    ("~/bin" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin" "/opt/local/bin" "/usr/local/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin")))
  '(git-gutter+-diff-options (quote ("--patience")))
  '(git-gutter+-hide-gutter t)
  '(github-clone-url-slot :clone-url)
@@ -51,7 +75,9 @@
  '(global-hl-line-mode t)
  '(global-pointback-mode t)
  '(global-undo-tree-mode t)
- '(hardhat-fullpath-protected-regexps (quote ("~/\\.cask/" "~/\\.emacs\\.d/\\.cask/" "~/\\.emacs\\.d/elpa/" "~/\\.cpan/" "~/\\.cabal/" "~/perl5/perlbrew/" "~/\\.npm/" "~/\\.virtualenv/" "~/\\.virthualenv/" "~/\\.rvm/" "/[._]build/" "/\\.bzr/" "/\\.coverage/" "/\\.git/" "/\\.hg/" "/\\.rspec/" "/\\.sass-cache/" "/\\.svn/" "/_MTN/" "/_darcs/" "/CVS/" "/pm_to_blib/" "/RCS/" "/SCCS/" "/blib/" "/test_output/")))
+ '(hardhat-fullpath-protected-regexps
+   (quote
+    ("~/\\.cask/" "~/\\.emacs\\.d/\\.cask/" "~/\\.emacs\\.d/elpa/" "~/\\.cpan/" "~/\\.cabal/" "~/perl5/perlbrew/" "~/\\.npm/" "~/\\.virtualenv/" "~/\\.virthualenv/" "~/\\.rvm/" "/[._]build/" "/\\.bzr/" "/\\.coverage/" "/\\.git/" "/\\.hg/" "/\\.rspec/" "/\\.sass-cache/" "/\\.svn/" "/_MTN/" "/_darcs/" "/CVS/" "/pm_to_blib/" "/RCS/" "/SCCS/" "/blib/" "/test_output/")))
  '(icomplete-mode t)
  '(ido-case-fold t)
  '(ido-confirm-unique-completion t)
@@ -59,9 +85,37 @@
  '(ido-enter-matching-directory (quote first))
  '(ido-everywhere t)
  '(ido-mode (quote both) nil (ido))
- '(ido-ubiquitous-command-overrides (quote ((disable exact "execute-extended-command") (enable prefix "wl-") (enable-old prefix "Info-") (enable exact "webjump") (enable regexp "\\`\\(find\\|load\\|locate\\)-library\\'"))))
+ '(ido-ubiquitous-command-overrides
+   (quote
+    ((disable exact "execute-extended-command")
+     (enable prefix "wl-")
+     (enable-old prefix "Info-")
+     (enable exact "webjump")
+     (enable regexp "\\`\\(find\\|load\\|locate\\)-library\\'")
+     (disable prefix "org-")
+     (disable prefix "magit-")
+     (disable prefix "tmm-")
+     (enable regexp "\\`\\(load\\|enable\\|disable\\|describe\\|custom-theme-visit\\)-theme\\'"))))
  '(ido-ubiquitous-enable-old-style-default nil)
- '(ido-ubiquitous-function-overrides (quote ((disable exact "read-file-name") (disable exact "read-file-name-internal") (disable exact "read-buffer") (disable exact "gnus-emacs-completing-read") (disable exact "gnus-iswitchb-completing-read") (disable exact "grep-read-files") (enable exact "bookmark-completing-read") (enable-old exact "webjump-read-choice") (enable-old exact "webjump-read-url-choice") (disable exact "isearchp-read-unicode-char"))))
+ '(ido-ubiquitous-function-overrides
+   (quote
+    ((disable exact "read-file-name")
+     (disable exact "read-file-name-internal")
+     (disable exact "read-buffer")
+     (disable exact "gnus-emacs-completing-read")
+     (disable exact "gnus-iswitchb-completing-read")
+     (disable exact "grep-read-files")
+     (enable exact "bookmark-completing-read")
+     (enable-old exact "webjump-read-choice")
+     (enable-old exact "webjump-read-url-choice")
+     (disable exact "isearchp-read-unicode-char")
+     (disable exact "org-completing-read")
+     (disable exact "org-completing-read-no-i")
+     (disable exact "org-iswitchb-completing-read")
+     (disable exact "org-icompleting-read")
+     (enable exact "read-char-by-name")
+     (disable exact "Info-read-node-name")
+     (disable exact "tmm-menubar"))))
  '(ido-ubiquitous-mode t)
  '(ido-use-virtual-buffers t)
  '(ido-yes-or-no-mode t)
@@ -75,7 +129,9 @@
  '(magit-diff-refine-hunk t)
  '(magit-repo-dirs (quote ("/Users/ryan/Projects")))
  '(magit-status-buffer-switch-function (quote switch-to-buffer))
- '(make-header-hook (quote (header-title header-blank header-file-name header-author header-copyright header-creation-date header-version header-pkg-requires header-url header-keywords header-compatibility header-blank header-lib-requires header-end-line header-commentary header-blank header-blank header-blank header-end-line header-free-software header-code header-provide-statement header-eof)))
+ '(make-header-hook
+   (quote
+    (header-title header-blank header-file-name header-author header-copyright header-creation-date header-version header-pkg-requires header-url header-keywords header-compatibility header-blank header-lib-requires header-end-line header-commentary header-blank header-blank header-blank header-end-line header-free-software header-code header-provide-statement header-eof)))
  '(marmalade-server "http://marmalade-repo.org" t)
  '(marmalade-token "jxzMp9uu6+16QPYKyarL9glfVfekWdWta8EEdMSkKlE=")
  '(midnight-mode t nil (midnight))
@@ -90,15 +146,95 @@
  '(org-adapt-indentation nil)
  '(org-support-shift-select t)
  '(pretty-symbol-categories (lambda relational nil))
- '(pretty-symbol-patterns (quote ((955 lambda "\\<lambda\\>" (emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode python-mode inferior-python-mode)) (402 lambda "\\<function\\>" (js-mode)) (8800 relational "!=" (c-mode c++-mode go-mode java-mode js-mode perl-mode cperl-mode ruby-mode python-mode inferior-python-mode)) (8800 relational "/=" (emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode)) (8805 relational ">=" (c-mode c++-mode go-mode java-mode js-mode perl-mode cperl-mode ruby-mode python-mode inferior-python-mode emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode)) (8804 relational "<=" (c-mode c++-mode go-mode java-mode js-mode perl-mode cperl-mode ruby-mode python-mode inferior-python-mode emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode)) (8743 logical "&&" (c-mode c++-mode go-mode java-mode js-mode perl-mode cperl-mode ruby-mode python-mode inferior-python-mode)) (8743 logical "\\<and\\>" (emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode)) (8744 logical "||" (c-mode c++-mode go-mode java-mode js-mode perl-mode cperl-mode ruby-mode python-mode inferior-python-mode)) (8744 logical "\\<or\\>" (emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode)) (172 logical "\\<not\\>" (emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode)) (8709 nil "\\<nil\\>" (emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode)))))
+ '(pretty-symbol-patterns
+   (quote
+    ((955 lambda "\\<lambda\\>"
+          (emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode python-mode inferior-python-mode))
+     (402 lambda "\\<function\\>"
+          (js-mode))
+     (8800 relational "!="
+           (c-mode c++-mode go-mode java-mode js-mode perl-mode cperl-mode ruby-mode python-mode inferior-python-mode))
+     (8800 relational "/="
+           (emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode))
+     (8805 relational ">="
+           (c-mode c++-mode go-mode java-mode js-mode perl-mode cperl-mode ruby-mode python-mode inferior-python-mode emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode))
+     (8804 relational "<="
+           (c-mode c++-mode go-mode java-mode js-mode perl-mode cperl-mode ruby-mode python-mode inferior-python-mode emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode))
+     (8743 logical "&&"
+           (c-mode c++-mode go-mode java-mode js-mode perl-mode cperl-mode ruby-mode python-mode inferior-python-mode))
+     (8743 logical "\\<and\\>"
+           (emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode))
+     (8744 logical "||"
+           (c-mode c++-mode go-mode java-mode js-mode perl-mode cperl-mode ruby-mode python-mode inferior-python-mode))
+     (8744 logical "\\<or\\>"
+           (emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode))
+     (172 logical "\\<not\\>"
+          (emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode))
+     (8709 nil "\\<nil\\>"
+           (emacs-lisp-mode inferior-lisp-mode lisp-mode scheme-mode)))))
  '(projectile-cache-file "/Users/ryan/.emacs.d/persistence/projectile.cache")
  '(projectile-enable-idle-timer t)
  '(projectile-global-mode nil)
- '(projectile-globally-ignored-modes (quote ("erc-mode" "help-mode" "completion-list-mode" "Buffer-menu-mode" "gnus-.*-mode" "occur-mode" "apropos-mode")))
+ '(projectile-globally-ignored-modes
+   (quote
+    ("erc-mode" "help-mode" "completion-list-mode" "Buffer-menu-mode" "gnus-.*-mode" "occur-mode" "apropos-mode")))
  '(projectile-known-projects-file "/Users/ryan/.emacs.d/persistence/projectile-bookmarks.eld")
- '(projectile-mode-line (quote (:eval (format " Proj[%s]" (projectile-project-name)))))
+ '(projectile-mode-line
+   (quote
+    (:eval
+     (format " Proj[%s]"
+             (projectile-project-name)))))
  '(require-final-newline t)
- '(safe-local-variable-values (quote ((flycheck-disabled-checkers (quote (emacs-lisp emacs-lisp-checkdoc))) (eval when (and (buffer-file-name) (file-regular-p (buffer-file-name)) (string-match-p "^[^.]" (buffer-file-name))) (emacs-lisp-mode) (when (fboundp (quote flycheck-mode)) (flycheck-mode -1)) (unless (featurep (quote package-build)) (let ((load-path (cons ".." load-path))) (require (quote package-build)))) (package-build-minor-mode)) (whitespace-style face tabs trailing lines-tail) (eval if (fboundp (quote pretty-symbols-mode)) (pretty-symbols-mode -1)) (c-indentation-style . bsd) (mangle-whitespace . t) (header-auto-update-enabled) (eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (whitespace-line-column . 80) (whitespace-style face trailing lines-tail) (require-final-newline . t) (outline-minor-mode) (whitespace-style face tabs spaces trailing lines space-before-tab::space newline indentation::space empty space-after-tab::space space-mark tab-mark newline-mark))))
+ '(safe-local-variable-values
+   (quote
+    ((flycheck-disabled-checkers
+      (quote
+       (emacs-lisp emacs-lisp-checkdoc)))
+     (eval when
+           (and
+            (buffer-file-name)
+            (file-regular-p
+             (buffer-file-name))
+            (string-match-p "^[^.]"
+                            (buffer-file-name)))
+           (emacs-lisp-mode)
+           (when
+               (fboundp
+                (quote flycheck-mode))
+             (flycheck-mode -1))
+           (unless
+               (featurep
+                (quote package-build))
+             (let
+                 ((load-path
+                   (cons ".." load-path)))
+               (require
+                (quote package-build))))
+           (package-build-minor-mode))
+     (whitespace-style face tabs trailing lines-tail)
+     (eval if
+           (fboundp
+            (quote pretty-symbols-mode))
+           (pretty-symbols-mode -1))
+     (c-indentation-style . bsd)
+     (mangle-whitespace . t)
+     (header-auto-update-enabled)
+     (eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook"
+           (add-hook
+            (quote write-contents-functions)
+            (lambda nil
+              (delete-trailing-whitespace)
+              nil))
+           (require
+            (quote whitespace))
+           "Sometimes the mode needs to be toggled off and on."
+           (whitespace-mode 0)
+           (whitespace-mode 1))
+     (whitespace-line-column . 80)
+     (whitespace-style face trailing lines-tail)
+     (require-final-newline . t)
+     (outline-minor-mode)
+     (whitespace-style face tabs spaces trailing lines space-before-tab::space newline indentation::space empty space-after-tab::space space-mark tab-mark newline-mark))))
  '(save-place t nil (saveplace))
  '(save-place-file "~/.emacs.d/persistence/.emacs-places")
  '(savehist-mode t)
@@ -106,7 +242,9 @@
  '(sentence-end-double-space nil)
  '(show-paren-mode t)
  '(sml-modeline-mode t)
- '(tempbuf-temporary-major-modes (quote (Custom-mode w3-mode Man-mode view-mode help-mode apropos-mode completion-list-mode dired-mode ibuffer-mode bookmark-bmenu-mode ess-help-mode compilation-mode vc-dir-mode vc-hg-log-view-mode occur-mode diff-mode magit-key-mode magit-log-edit-mode ess-help-mode)))
+ '(tempbuf-temporary-major-modes
+   (quote
+    (Custom-mode w3-mode Man-mode view-mode help-mode apropos-mode completion-list-mode dired-mode ibuffer-mode bookmark-bmenu-mode ess-help-mode compilation-mode vc-dir-mode vc-hg-log-view-mode occur-mode diff-mode magit-key-mode magit-log-edit-mode ess-help-mode)))
  '(tool-bar-mode nil)
  '(tramp-default-method "sftp")
  '(undo-tree-auto-save-history t)
