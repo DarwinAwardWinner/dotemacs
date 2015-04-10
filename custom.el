141a2d8536c6528407f8e787da68d88f9088a887
Ryan C. Thompson <rct@thompsonclan.org>
2015-04-09 22:40:01 -0700

    Add entry to safe-local-variable-values

--- custom.el
+++ custom.el
@@ -187,7 +187,40 @@
  '(require-final-newline t)
  '(safe-local-variable-values
    (quote
-    ((flycheck-disabled-checkers
+    ((eval when
+           (and
+            (buffer-file-name)
+            (file-regular-p
+             (buffer-file-name))
+            (string-match-p "^[^.]"
+                            (buffer-file-name)))
+           (emacs-lisp-mode)
+           (when
+               (fboundp
+                (quote flycheck-mode))
+             (flycheck-mode -1))
+           (unless
+               (featurep
+                (quote package-build))
+             (let
+                 ((load-path
+                   (cons ".." load-path)))
+               (require
+                (quote package-build))))
+           (package-build-minor-mode)
+           (set
+            (make-local-variable
+             (quote package-build-working-dir))
+            (expand-file-name "../working/"))
+           (set
+            (make-local-variable
+             (quote package-build-archive-dir))
+            (expand-file-name "../packages/"))
+           (set
+            (make-local-variable
+             (quote package-build-recipes-dir))
+            default-directory))
+     (flycheck-disabled-checkers
       (quote
        (emacs-lisp emacs-lisp-checkdoc)))
      (eval when
