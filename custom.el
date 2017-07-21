25f8472b9f6643105870be9d02316c3a5782736e
Ryan C. Thompson <rct@thompsonclan.org>
2017-07-21 12:03:26 -0700

    Switch to Fira Code font
    Ligatures are not enabled due to a bug in Emacs

--- custom.el
+++ custom.el
@@ -364,6 +364,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Fira Code"))))
  '(auto-dim-other-buffers-face ((t (:background "gray97"))))
  '(git-gutter+-added ((t (:foreground "green4" :weight bold))))
  '(git-gutter+-deleted ((t (:foreground "red4" :weight bold))))
