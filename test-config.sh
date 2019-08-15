b061dfa0e9541ddcd83cc9543215f43146007820
Ryan C. Thompson <rct@thompsonclan.org>
2019-08-14 21:38:56 -0700

    Allow travis failures
    These failures are mainly due to TLS weirdness and other Emacs issues,
    not any problem with the config itself.

--- test-config.sh
+++ test-config.sh
@@ -7,8 +7,10 @@ rm -f "$TMPHOME/.emacs.d/config.el"{,c}
 cp config.org init.el custom.el "$TMPHOME/.emacs.d/"
 HOME="$TMPHOME" \
     emacs "$@" \
+    --eval '(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")' \
     --eval "(setq after-init-time nil)" \
     --eval "(setq network-security-level 'low)" \
     -f toggle-debug-on-error \
     -l "~/.emacs.d/init.el" \
     --eval "(wsi-simulate-idle-time 500)"
+# Note: the TLS thing works around Emacs bug #34341
