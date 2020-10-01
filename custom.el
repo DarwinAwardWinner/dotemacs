7211f50ed1b3021b16e59c3051b56879d774c3d7
Ryan C. Thompson <rct@thompsonclan.org>
2020-09-30 21:51:43 -0400

    Set a height for default face

--- custom.el
+++ custom.el
@@ -458,7 +458,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono"))))
+ '(default ((t (:height 110 :family "DejaVu Sans Mono"))))
  '(auto-dim-other-buffers-face ((t (:background "gray97"))))
  '(git-gutter+-added ((t (:foreground "green4" :weight bold))))
  '(git-gutter+-deleted ((t (:foreground "red4" :weight bold))))
