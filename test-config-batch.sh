653a89344b2bb387957b8afd6c5770360fa5f671
Ryan C. Thompson <rct@thompsonclan.org>
2021-02-06 09:19:11 -0500

    Fix faulty "load twice" logic
    The CI tests now load the config twice while re-using the same home
    directory, which was the original intent.

---
+++ test-config-batch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euo pipefail
+
+TMPHOME="$(mktemp -d)"
+
+mkdir -p "$TMPHOME/.emacs.d/"
+rm -f "$TMPHOME/.emacs.d/config.el"{,c}
+cp config.org init.el custom.el "$TMPHOME/.emacs.d/"
+
+echo "Loading config for the 1st time"
+
+HOME="$TMPHOME" \
+    emacs -batch \
+    --eval "(setq after-init-time nil)" \
+    --eval "(setq network-security-level 'low)" \
+    -f toggle-debug-on-error \
+    -l "~/.emacs.d/init.el" \
+    --eval "(wsi-simulate-idle-time 500)" \
+    --eval "(require 'ido-completing-read+)"
+
+echo "Loading config for the 2nd time. It should not need to install any more packages."
+HOME="$TMPHOME" \
+    emacs -batch \
+    --eval "(setq after-init-time nil)" \
+    --eval "(setq network-security-level 'low)" \
+    -f toggle-debug-on-error \
+    -l "~/.emacs.d/init.el" \
+    --eval "(wsi-simulate-idle-time 500)" \
+    --eval "(require 'ido-completing-read+)"
