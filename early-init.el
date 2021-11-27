57a007dd4d8a0afdc64c813d6305846cc35081a1
Ryan C. Thompson <rct@thompsonclan.org>
2021-11-27 14:45:56 -0500

    Disable GC during init

--- early-init.el
+++ early-init.el
@@ -20,6 +20,18 @@
 ;;
 ;;; Code:
 
+;; Disable GC during init
+(put 'gc-cons-percentage 'original-value-before-init gc-cons-percentage)
+(put 'gc-cons-percentage 'value-during-init 0.6)
+(defun restore-gc-cons-percentage-after-init ()
+  (let ((expected-value (get 'gc-cons-percentage 'value-during-init))
+        (value-to-restore (get 'gc-cons-percentage 'original-value-before-init)))
+    (when (and value-to-restore (equal gc-cons-percentage expected-value))
+      (message "Setting `gc-cons-percentage' back to starting value %s" value-to-restore)
+      (setq gc-cons-percentage value-to-restore))))
+(add-hook 'after-init-hook #'restore-gc-cons-percentage-after-init)
+(setq gc-cons-percentage (get 'gc-cons-percentage 'value-during-init))
+
 ;; Recommended by
 ;; https://github.com/raxod502/straight.el#getting-started to prevent
 ;; pacakge.el stepping on straight's toes.
