900a406be85b16b28b122228ea2a08a48486ae6f
Ryan C. Thompson <rct@thompsonclan.org>
2023-09-17 16:45:46 -0400

    Slightly tweak code for disabling GC during init

--- early-init.el
+++ early-init.el
@@ -20,15 +20,20 @@
 ;;
 ;;; Code:
 
-;; Disable GC during init
+;; Disable GC during init, which speeds up the init process. After
+;; init, we return the GC parameters to their original state, unless
+;; something else has changed them in the meantime.
 (put 'gc-cons-percentage 'original-value-before-init gc-cons-percentage)
 (put 'gc-cons-percentage 'value-during-init 0.6)
 (defun restore-gc-cons-percentage-after-init ()
   (let ((expected-value (get 'gc-cons-percentage 'value-during-init))
         (value-to-restore (get 'gc-cons-percentage 'original-value-before-init)))
-    (when (and value-to-restore (equal gc-cons-percentage expected-value))
+    (when (and expected-value value-to-restore (equal gc-cons-percentage expected-value))
       (message "Setting `gc-cons-percentage' back to starting value %s" value-to-restore)
-      (setq gc-cons-percentage value-to-restore))))
+      (setq gc-cons-percentage value-to-restore)))
+  ;; Clean up the properties, which are no longer needed
+  (put 'gc-cons-percentage 'original-value-before-init nil)
+  (put 'gc-cons-percentage 'value-during-init nil))
 (add-hook 'after-init-hook #'restore-gc-cons-percentage-after-init)
 (setq gc-cons-percentage (get 'gc-cons-percentage 'value-during-init))
 
