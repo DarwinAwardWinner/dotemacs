472705188832aa5f804e3e81c87a0f83a1074321
Ryan C. Thompson <ryan.thompson2@mssm.edu>
2026-07-22 14:56:23 -0400

    Update to latest bootstrap code for straight.el

--- init.el
+++ init.el
@@ -22,16 +22,21 @@
 ;; Bootstrap straight package manager
 (defvar bootstrap-version)
 (let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
+
+(require 'straight)
 
 ;; (Try to) Ensure the latest org-mode is installed
 (condition-case nil
